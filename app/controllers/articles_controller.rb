fb274775d26cab1639d5c7cce35bd09a25c588eb
ASLAN USSAYEV <usprof777@gmail.com>
2015-10-01 12:37:55 -0500

    Some view improvements

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
 	before_action :find_article, only: [:show, :edit, :update, :destroy]
-	before_action :authenticate_user!, except: [:index, :show]
+	before_action :authenticate_user!, except: [:index, :show, :edit, :update]
 
 	def index
 		if params[:category].blank?
